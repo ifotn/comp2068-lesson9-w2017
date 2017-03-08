07c27493bd97aa1a9527834339df529ef2bbda29
ifotn <rich@infrontofthenet.com>
2017-03-08 13:48:21 -0500

    end of class files

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// passport dependencies
+let passport = require('passport');
+let session = require('express-session');
+let localStrategy = require('passport-local').Strategy;
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 // reference the books controller we created
@@ -33,6 +38,25 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+// configure passport and sessions
+app.use(session({
+  secret: 'some salt value here',
+    resave: true,
+    saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// link to the new Account model
+let Account = require('./models/account');
+passport.use(Account.createStrategy());
+
+// manage user login status through the db
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// routing goes after passport config so controllers can use passport
 app.use('/', index);
 app.use('/users', users);
 app.use('/books', books); // handle all requests at /books with books router
@@ -52,7 +76,9 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    title: 'COMP2068 - Book Store'
+  });
 });
 
 module.exports = app;
--- routes/books.js
+++ routes/books.js
@@ -8,6 +8,15 @@ let router = express.Router();
 // link to the book model for CRUD operations
 let Book = require('../models/book');
 
+// auth check
+function isLoggedIn(req, res, next) {
+   if (req.isAuthenticated()) {
+      return next(); // user is logged, so call the next function
+   }
+
+   res.redirect('/'); // not logged in so redirect to home
+}
+
 /* GET books main page */
 router.get('/', function(req, res, next) {
 
@@ -22,21 +31,23 @@ router.get('/', function(req, res, next) {
       // no error so send the books to the index view
       res.render('books/index', {
          books: books,
-         title: 'Book List'
+         title: 'Book List',
+          user: req.user
       });
    });
 });
 
 // GET /books/add - show blank add form
-router.get('/add', function(req, res, next) {
+router.get('/add', isLoggedIn,  function(req, res, next) {
    // show the add form
    res.render('books/add', {
-      title: 'Book Details'
+      title: 'Book Details',
+       user: req.user
    });
 });
 
 // POST /books/add - save the new book
-router.post('/add', function(req, res, next) {
+router.post('/add', isLoggedIn, function(req, res, next) {
    // use Mongoose to populate a new Book
    Book.create({
       title: req.body.title,
@@ -54,7 +65,7 @@ router.post('/add', function(req, res, next) {
 });
 
 // GET /books/delete/_id - delete and refresh the index view
-router.get('/delete/:_id', function(req, res, next) {
+router.get('/delete/:_id', isLoggedIn, function(req, res, next) {
    // get the id parameter from the end of the url
    let _id = req.params._id;
 
@@ -70,7 +81,7 @@ router.get('/delete/:_id', function(req, res, next) {
 });
 
 // GET /books/_id - show edit page and pass it the selected book
-router.get('/:_id', function(req, res, next) {
+router.get('/:_id', isLoggedIn, function(req, res, next) {
    // grab id from the url
    let _id = req.params._id;
 
@@ -83,13 +94,14 @@ router.get('/:_id', function(req, res, next) {
       }
       res.render('books/edit', {
          book: book,
-         title: 'Book Details'
+         title: 'Book Details',
+          user: req.user
       });
    });
 });
 
 // POST /books/_id - save the updated book
-router.post('/:_id', function(req, res, next) {
+router.post('/:_id', isLoggedIn, function(req, res, next) {
    // grab id from url
    let _id = req.params._id;
 
